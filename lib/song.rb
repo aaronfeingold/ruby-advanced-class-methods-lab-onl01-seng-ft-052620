5cd8d1eb8e26f4467e3ed4c5b465a7bb62fc6734
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-06 01:57:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
   end
   
   def self.create_from_filename(file_name)
-    song = self.new_from_filename(file-name)
+    song = self.new_from_filename(file_name)
     song.save
     song
   end
