e4e665c3e94f81b559ff5e7f3c18b2b14c7aaa1f
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-06 01:55:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,6 @@ class Song
   
   def self.new_from_filename(file_name)
     artist_name, song_name = file_name.split(" - ")
-    binding.pry
     song = self.new_by_name(song_name.gsub(".mp3", "")
     song.artrist_name = artist_name
     song 
